758963ff447d7e3601cfd1e058a3face89c5f75b
meri <mdiaz>
2020-05-01 23:45:55 +0200

    creando tests

--- conciertos/tests.py
+++ conciertos/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
 # Create your tests here.
+class TestViews(TestCase):
+    def test_barra(self):
+        check = '<img src="/static/conciertos/black_album.jpg" width="200" height="200" align="right">'
+        response = self.client.get('/conciertos/')
+        content = response.content.decode(encoding='UTF-8')
+        self.assertInHTML(check, content)
+
+    def test_nav(self):
+        check = '<a class="nav-link" href="/conciertos/grupos/">Grupos</a>'
+        response = self.client.get('/conciertos/grupos')
+        content = response.content.decode(encoding='UTF-8')
+        self.assertInHTML(check, content)
--- conciertos/views.py
+++ conciertos/views.py
@@ -58,13 +58,13 @@ def musico(request, id=0):
 def concierto(request, id=0):
     if request.method == "POST":
         form = ConciertoForm(request.POST)
-        print(form.is_valid())
-        print(form.cleaned_data['hora'])
         if form.is_valid():
             concierto = Concierto(fecha=datetime.combine(form.cleaned_data['fecha'], form.cleaned_data['hora']),
                             lugar=form.cleaned_data['lugar'], grupo=form.cleaned_data['grupo'])
             concierto.save()
             return redirect('concierto', id=concierto.id)
+        else:
+            return HttpResponse('No has introducido correctamente los datos')
     else:
         concierto = get_object_or_404(Concierto, id=id)
     return render(request, 'conciertos/content.html', {'contenido': concierto})
